a4110b62ec2c2667eb16afe3c887f5bba9bb8423
Antonio <84136604+JinZayex@users.noreply.github.com>
2024-05-04 02:33:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,28 @@ fetch('biblio.json')
     .then(data => {
         console.log("my data",data); // Effettua il log dei dati su console
 
-        data.forEach(function(element){
+        data.forEach(function(element) {
             var h3 = document.createElement("h3");          // Create a new h1 element
-            h3.textContent = element.nome;                // Set the content of the h1 element to the name of the current element
+            h3.textContent = element.titolo;                // Set the content of the h1 element to the name of the current element
             document.body.appendChild(h3);                  // Append the h1 element to the body (you can append it to any other element you want)
+        
+            var p = document.createElement("p");          // Create a new h1 element
+            p.textContent = element.autore;                 // Set the content of the h1 element to the name of the current element
+            document.body.appendChild(p);   
+        
+            var p = document.createElement("p");          // Create a new h1 element
+            p.textContent = "da: " + element.contributore;                 // Set the content of the h1 element to the name of the current element
+            document.body.appendChild(p);   
+        
+            var a = document.createElement("a");
+            a.href = element.link;
+            a.download = element.titolo;
+            a.textContent = "Download " + element.titolo; 
+            document.body.appendChild(a)
+        
+            var hr = document.createElement("hr");          // Create a new h1 element
+            document.body.appendChild(hr);   
+        
         })
     })
     .catch(error => console.error('Errore:', error));
