805047cfc436cdd322e1b8564fe8a4261be6717f
Antonio <84136604+JinZayex@users.noreply.github.com>
2024-05-04 02:29:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 fetch('biblio.json')
     .then(response => response.json()) // Converti la risposta in formato JSON
     .then(data => {
-        console.log(data); // Effettua il log dei dati su console
+        console.log("my data",data); // Effettua il log dei dati su console
 
         data.forEach(function(element){
             var h3 = document.createElement("h3");          // Create a new h1 element
